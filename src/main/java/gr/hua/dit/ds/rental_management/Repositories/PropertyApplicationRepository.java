5ed3640a411cd1d242beff2c685683b0eb9437e5
it22129 <92916088+it22129@users.noreply.github.com>
2025-01-17 19:31:39 +0200

    Delete src directory

--- src/main/java/gr/hua/dit/ds/rental_management/Repositories/PropertyApplicationRepository.java
+++
@@ -1,7 +0,0 @@
-package gr.hua.dit.ds.rental_management.Repositories;
-
-import gr.hua.dit.ds.rental_management.Entities.PropertyApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PropertyApplicationRepository extends JpaRepository<PropertyApplication, Integer> {
-}
